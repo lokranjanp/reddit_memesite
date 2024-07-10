90a1fd75cd6b0d4475d3b5dbbd6eda50d325300d
lokranjanp <lokranjan03@gmail.com>
2024-07-10 15:36:51 +0530

    patch fix 3

--- app.py
+++ app.py
@@ -3,31 +3,33 @@ import praw
 import random
 import os
 
-app = Flask(__name__)   #initialised new flask app
+app = Flask(__name__)   # initialised new flask app
 
-#loading .env credentials
+# loading .env credentials
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 user_agent = os.getenv('USER_AGENT')
 
-#creating the praw object.
+# creating the praw object.
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
     user_agent=user_agent
 )
 
+
 @app.route('/')
 def home():
-    subreddit = reddit.subreddit('memes')   #fetch subreddit
-    submissions = list(subreddit.hot(limit=690))    #fetch memes
-    submission = random.choice(submissions)     #choose random meme
+    subreddit = reddit.subreddit('memes')   # fetch subreddit
+    submissions = list(subreddit.hot(limit=690))    # fetch memes
+    submission = random.choice(submissions)     # choose random meme
     meme = {
         'title': submission.title,
         'url': submission.url,
         'permalink': submission.permalink
     }
     return render_template('index.html', meme=meme, countdown=30)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
