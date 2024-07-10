bc01e9ae906b27df66ac3d8eae3c5ac4b245e581
lokranjanp <lokranjan03@gmail.com>
2024-07-10 13:47:22 +0530

    add container, other func

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template
-import requests
-import json
+import praw
+import random
+import os
 
 app = Flask(__name__)
 
-def get_meme():
-    url = "https://meme-api.herokuapp.com/gimme"
-    response = json.loads(requests.get(url).text)
-    meme_large = response['preview'][-2]
-    subreddit = response['subreddit']
-    return meme_large, subreddit
+# Reddit API credentials
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+user_agent = os.getenv('USER_AGENT')
 
-@app.route("/")
-def index():
-    meme_large, subreddit = get_meme()
-    return render_template("index.html", meme_pic=meme_large, subreddit=subreddit)
+# Initialize PRAW
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent
+)
 
+@app.route('/')
+def home():
+    subreddit = reddit.subreddit('memes')
+    # Get a list of hot submissions
+    submissions = list(subreddit.hot(limit=50))
+    # Select a random submission
+    submission = random.choice(submissions)
+    meme = {
+        'title': submission.title,
+        'url': submission.url,
+        'permalink': submission.permalink
+    }
+    return render_template('index.html', meme=meme, countdown=30)
 
+if __name__ == '__main__':
+    app.run(debug=True)
