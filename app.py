df8f478a203aea15fda782fa41981a71d3f5654b
lokranjanp <lokranjan03@gmail.com>
2024-07-10 13:07:48 +0530

    basic ready

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
+def get_meme():
+    url = "https://meme-api.herokuapp.com/gimme"
+    response = json.loads(requests.get(url).text)
+    meme_large = response['preview'][-2]
+    subreddit = response['subreddit']
+    return meme_large, subreddit
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def index():
+    meme_large, subreddit = get_meme()
+    return render_template("index.html", meme_pic=meme_large, subreddit=subreddit)
 
 
-if __name__ == '__main__':
-    app.run()
